999e0cebd7d0536790e4f0154266d25a7b1d8e6f
olivia pasion <oap9820@gmail.com>
2022-06-03 11:29:55 -0700

    evolution select tested and working

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 // grab DOM elements
 
+const nameInput = document.getElementById('name-input');
+const nameDisplay = document.getElementById('name-display');
+
+const evolutionSelect = document.getElementById('evolution-select');
+const imageDisplay = document.getElementById('image-display');
+
+const moveSelect = document.getElementById('move-select');
+const pokeCard = document.getElementById('poke-card');
+
+
+const exportButton = document.getElementById('export-button');
+
+
 // set event listeners
+
+exportButton.addEventListener('click', () => {
+
+});
+
+evolutionSelect.addEventListener('change', () => {
+    const src = 'assets/pokemon/' + evolutionSelect.value;
+    imageDisplay.src = src;
+
+});
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
